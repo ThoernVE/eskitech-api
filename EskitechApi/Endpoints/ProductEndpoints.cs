eec93c518c13b13efeb982493d2b281602581440
Thoernve <viktor.e.thorn@hotmail.com>
2025-09-20 18:09:47 +0200

    Added Name, Summary and Description to Endpoints and removed last part of boilerplate-code inside of program.cs

--- EskitechApi/Endpoints/ProductEndpoints.cs
+++ EskitechApi/Endpoints/ProductEndpoints.cs
@@ -26,7 +26,10 @@ namespace EskitechApi.Endpoints
                     logger.LogError(ex, "Failed to fetch products from Excelfile");
                     return Results.StatusCode(500);
                 }
-            });
+            })
+            .WithName("GetProductsFromExcel")
+            .WithSummary("Fetch all products from Excel-file")
+            .WithDescription("Retrieves a full list of products from the Excelfile currently in the datafolder. Retrieves empty list if no products are found. Returns 404 if file source is missing and 500 if something unexpected went wrong");
 
             app.MapGet("/products/excel/names", (IProductService productService, ILogger<Program> logger) =>
             {
@@ -45,7 +48,10 @@ namespace EskitechApi.Endpoints
                     logger.LogError(ex, "Failed to fetch products from Excelfile");
                     return Results.StatusCode(500);
                 }
-            });
+            })
+            .WithName("GetProductnamesFromExcel")
+            .WithSummary("Fetch all productnames from Excel-file")
+            .WithDescription("Retrieves a full list of only productnames from the Excelfile currently in the datafolder. Retrieves empty list if no products are found. Retrieves empty list if no products are found. Returns 404 if file source is missing and 500 if something unexpected went wrong");
 
             app.MapGet("/products/excel/prices", (IProductService productService, ILogger<Program> logger) =>
             {
@@ -64,7 +70,10 @@ namespace EskitechApi.Endpoints
                     logger.LogError(ex, "Failed to fetch products from Excelfile");
                     return Results.StatusCode(500);
                 }
-            });
+            })
+            .WithName("GetProductPricesFromExcel")
+            .WithSummary("Fetch all productnames with prices from Excel-file")
+            .WithDescription("Retrieves a full list of productnames and prices from the Excelfile currently in the datafolder. Retrieves empty list if no products are found. Returns 404 if file source is missing and 500 if something unexpected went wrong");
 
             app.MapGet("/products/excel/stock", (IProductService productService, ILogger<Program> logger) =>
             {
@@ -83,7 +92,10 @@ namespace EskitechApi.Endpoints
                     logger.LogError(ex, "Failed to fetch products from Excelfile");
                     return Results.StatusCode(500);
                 }
-            });
+            })
+            .WithName("GetProductStockFromExcel")
+            .WithSummary("Fetch all productnames with stock from Excel-file")
+            .WithDescription("Retrieves a full list of productnames and stock from the Excelfile currently in the datafolder. Retrieves empty list if no products are found. Returns 404 if file source is missing and 500 if something unexpected went wrong");
 
             app.MapGet("/products/excel/count", (IProductService productService, ILogger<Program> logger) =>
             {
@@ -102,7 +114,10 @@ namespace EskitechApi.Endpoints
                     logger.LogError(ex, "Failed to fetch products from Excelfile");
                     return Results.StatusCode(500);
                 }
-            });
+            })
+            .WithName("GetProductCountFromExcel")
+            .WithSummary("Get count of all products from Excel-file")
+            .WithDescription("Retrieves a count of products from the Excelfile currently in the datafolder. Retrieves 0 if no products are found. Returns 404 if file source is missing and 500 if something unexpected went wrong");
 
             //Add products to DB from excel
             app.MapPost("/products/upload", async (IExcelService excelService, ILogger<Program> logger, IFormFile file) =>
@@ -120,7 +135,10 @@ namespace EskitechApi.Endpoints
                 }
             })
             .Accepts<IFormFile>("multipart/form-data")
-            .DisableAntiforgery();
+            .DisableAntiforgery()
+            .WithName("UploadExcelToDatabase")
+            .WithSummary("Upload Excel-file to the database")
+            .WithDescription("Uploads a Excel-file to a SQL database. Does not upload products with the same name. Returns 400 if file cannot be uploaded");
 
             //DB endpoints
             app.MapGet("/products/db", (IProductService productService, ILogger<Program> logger) =>
@@ -135,7 +153,10 @@ namespace EskitechApi.Endpoints
                     logger.LogError(ex, "Failed to fetch products from database");
                     return Results.StatusCode(500);
                 }
-            });
+            })
+            .WithName("GetProductsFromDatabase")
+            .WithSummary("Fetch all products from the database")
+            .WithDescription("Retrieves a full list of products from the database. Retrieves empty list if no products are found. Returns 500 if something unexpected went wrong");
 
             app.MapGet("/products/db/paginated", (IProductService productService, ILogger<Program> logger, int page = 1, int pageSize = 10) =>
             {
@@ -153,7 +174,10 @@ namespace EskitechApi.Endpoints
                     logger.LogError(ex, "Failed to fetch products from database");
                     return Results.StatusCode(500);
                 }
-            });
+            })
+            .WithName("GetPaginatedProductsFromDatabase")
+            .WithSummary("Fetch products from the database with pagination")
+            .WithDescription("Retrieves a subset of products from the database using paging parameters. Provide a `page` number (starting at 1) and a `pageSize` (greater than 0). If page or pageSize are invalid, a 400 Bad Request is returned. If the requested page exceeds the number of products available, an empty list is returned. Returns 500 if something unexpected goes wrong.");
 
             app.MapGet("/products/db/names", (IProductService productService, ILogger<Program> logger) =>
             {
@@ -167,7 +191,10 @@ namespace EskitechApi.Endpoints
                     logger.LogError(ex, "Failed to fetch products from database");
                     return Results.StatusCode(500);
                 }
-            });
+            })
+            .WithName("GetProductNamesFromDatabase")
+            .WithSummary("Fetch all productnames from the database")
+            .WithDescription("Retrieves a full list of productnames from the database. Retrieves empty list if no products are found. Returns 500 if something unexpected went wrong");
 
             app.MapGet("/products/db/prices", (IProductService productService, ILogger<Program> logger) =>
             {
@@ -181,7 +208,10 @@ namespace EskitechApi.Endpoints
                     logger.LogError(ex, "Failed to fetch products from database");
                     return Results.StatusCode(500);
                 }
-            });
+            })
+            .WithName("GetProductPricesFromDatabase")
+            .WithSummary("Fetch all productnames and prices from the database")
+            .WithDescription("Retrieves a full list of productnames and prices from the database. Retrieves empty list if no products are found. Returns 500 if something unexpected went wrong");
 
             app.MapGet("/products/db/stock", (IProductService productService, ILogger<Program> logger) =>
             {
@@ -195,7 +225,10 @@ namespace EskitechApi.Endpoints
                     logger.LogError(ex, "Failed to fetch products from database");
                     return Results.StatusCode(500);
                 }
-            });
+            })
+            .WithName("GetProductStockFromDatabase")
+            .WithSummary("Fetch all productnames and stock from the database")
+            .WithDescription("Retrieves a full list of productnames and stock from the database. Retrieves empty list if no products are found. Returns 500 if something unexpected went wrong");
 
             app.MapGet("/products/db/count", (IProductService productService, ILogger<Program> logger) =>
             {
@@ -209,7 +242,10 @@ namespace EskitechApi.Endpoints
                     logger.LogError(ex, "Failed to fetch products from database");
                     return Results.StatusCode(500);
                 }
-            });
+            })
+            .WithName("GetProductCountFromDatabase")
+            .WithSummary("Fetch count of all products in database")
+            .WithDescription("Retrieves a count products from the database. Retrieves 0 if no products are found. Returns 500 if something unexpected went wrong");
         }
     }
 }
--- EskitechApi/Program.cs
+++ EskitechApi/Program.cs
@@ -53,8 +53,3 @@ app.UseHttpsRedirection();
 app.MapProductEndpoints();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
